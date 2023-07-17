62d5fcab9824040ea8d685ca5e092ac51c347270
neverwinter-sjh <neverwinter.sjh@gmail.com>
2023-07-17 19:40:07 +0900

    fix

--- pages/index.js
+++ pages/index.js
@@ -42,16 +42,16 @@ export default function Home() {
 
       <main className={styles.main}>
         <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>동물 명을 입력하세요.</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
             name="animal"
-            placeholder="Enter an animal"
+            placeholder="동물 명 입력"
             value={animalInput}
             onChange={(e) => setAnimalInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="영웅적인 이름 짓기" />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
